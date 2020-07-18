d9672a0bec1d83edee7e5e650bb500467c2a6868
al xayeed <alxayeed@gmail.com>
2020-07-18 17:01:12 +0600

    placeholder added

--- todo/task/forms.py
+++ todo/task/forms.py
@@ -4,6 +4,7 @@ from django.forms import ModelForm
 from .models import *
 
 class TasksForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Add Something new to Your Bucket List!'}))
 
     class Meta:
         model = Tasks
