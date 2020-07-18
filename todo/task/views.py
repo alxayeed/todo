2c56a66ff091968925e8391144f5e1348ed0b165
al xayeed <alxayeed@gmail.com>
2020-07-18 16:32:38 +0600

    Update and delete functionality is added

--- todo/task/views.py
+++ todo/task/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
@@ -14,6 +14,34 @@ def index(request):
         form = TasksForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('/')
 
     context = {'tasks': tasks, 'form':form}
-    return render(request,'task/list.html', context)
+    return render(request, 'task/list.html', context)
+    
+
+def updateTask(request, pk):
+    task = Tasks.objects.get(id=pk)
+
+    form = TasksForm(instance=task)
+
+    if request.method == 'POST':
+        form = TasksForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form':form}
+    return render(request, 'task/update_task.html', context)
+
+def deleteTask(request, pk):
+    item = Tasks.objects.get(id=pk)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect('/')
+
+
+    context = {'item':item}
+
+    return render(request, 'task/delete.html', context)
