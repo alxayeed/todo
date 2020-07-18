5a012e9ee4d7844c00e9bacd8c68134d58e359e6
al xayeed <alxayeed@gmail.com>
2020-07-18 15:18:00 +0600

    forms added.html file modied accordingly

--- todo/task/views.py
+++ todo/task/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import *
+from .forms import *
 
 # Create your views here.
 def index(request):
     tasks = Tasks.objects.all()
 
-    context = {'tasks': tasks}
-    return render(request,'task/list.html', context=tasks)
+    form = TasksForm()
+
+    if request.method == 'POST':
+        form = TasksForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'tasks': tasks, 'form':form}
+    return render(request,'task/list.html', context)
